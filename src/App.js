02f2c658494ac8ddb216b370f4d09efdb811b46e
Onkar <you@example.com>
2024-10-28 19:42:01 -0400

    added icons for linkedin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects.js';
 import Experience from './pages/Experience.js';
 import Navbar from './components/navbar.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path = "/Projects" element={<Projects />}/>
           <Route path = "/experience" element={<Experience />}/>
         </Routes>
+        <Footer/>
       </Router>
       
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import EmailIcon from "@material-ui/icons/Instagram"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'></div>
+      <h2>Hi, my name is Onkar.</h2>
+      <div className='prompt'></div>
+      <p>
+        A software developer with a passion for learning and creating.
+      </p>
+      <LinkedInIcon/>
+      <EmailIcon/>
+      <GitHubIcon/>
+
+
+
+      <div className='skills'></div>
+    </div>
   )
 }
 
