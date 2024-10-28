f3a74e09cbf9468e555ea5b97b75d86895933501
Onkar <you@example.com>
2024-10-28 19:44:03 -0400

    added skills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import LinkedInIcon from "@material-ui/icons/LinkedIn"
-import EmailIcon from "@material-ui/icons/Instagram"
-import GitHubIcon from "@material-ui/icons/GitHub"
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Instagram";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='about'></div>
+    <div className="home">
+      <div className="about"></div>
       <h2>Hi, my name is Onkar.</h2>
-      <div className='prompt'></div>
-      <p>
-        A software developer with a passion for learning and creating.
-      </p>
-      <LinkedInIcon/>
-      <EmailIcon/>
-      <GitHubIcon/>
+      <div className="prompt"></div>
+      <p>A software developer with a passion for learning and creating.</p>
+      <LinkedInIcon />
+      <EmailIcon />
+      <GitHubIcon />
 
-
-
-      <div className='skills'></div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
+              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
+              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+          </li>
+        </ol>
+        </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
