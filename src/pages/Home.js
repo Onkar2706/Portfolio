d87ee0964e9ad11bf470145479ee48f59fdc2014
Onkar <you@example.com>
2024-11-06 23:24:30 -0500

    resume and projects added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import "../styles/Home.css";
+import ViewResume from "../components/ViewResume";
 
 function Home() {
   return (
@@ -33,6 +34,7 @@ function Home() {
             <GitHubIcon />
           </a>
         </div>
+        <ViewResume/>
       </div>
 
       <div className="skills">
