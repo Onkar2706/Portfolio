a13c02cda500b12408d6ab6fd46892ad983de1ac
Onkar <you@example.com>
2024-12-18 12:44:27 +0530

    added some lines

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2>Hi, I am Onkar. Welcome to my page.</h2>
+        <h2>Hi, I am Onkar.</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
         </div>
